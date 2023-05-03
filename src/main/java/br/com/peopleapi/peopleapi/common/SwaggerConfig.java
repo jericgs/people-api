4882e9bccb14c595f5b037eedeb9f94488f7baf0
Erico Gomes <jerick.gs@gmail.com>
2023-05-02 21:42:06 -0300

    Add Swagger.

---
+++ src/main/java/br/com/peopleapi/peopleapi/common/SwaggerConfig.java
@@ -0,0 +1,21 @@
+package br.com.peopleapi.peopleapi.common;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+
+@RestController
+@OpenAPIDefinition(info =
+@Info(
+    title = "Demo",
+    version = "1.0",
+    description = "Documentation for our SBDemo API Application",
+    license = @License(name = "Apache 2.0", url = "http://foo.bar"),
+    contact = @Contact(url = "https://www.thomasjayconsulting.com", name = "Tom", email = "tom@thomasjayconsulting.com")
+))
+public class SwaggerConfig {
+
+}
